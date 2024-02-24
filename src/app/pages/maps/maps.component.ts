14523ef4443f39b6166c5849aafeabdeb5ed6184
Brian-Sebastian1 <Brian.Sebastian1@ibm.com>
2024-02-24 18:18:01 +0530

    Metrics section

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import Chart from 'chart.js';
 
 declare var google: any;
 //declare var tableData: any;
@@ -12,6 +13,11 @@ declare var google: any;
 
 export class MapsComponent implements OnInit {
   public tableData: any;
+  public canvas : any;
+  public ctx;
+  public chartColor;
+  public chartEmail;
+  public chartHours;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -28,8 +34,10 @@ export class MapsComponent implements OnInit {
       "data": []
     }
 
+
     // API Call
 		let headers = new HttpHeaders({});
+
 		this.http
 			.get<any>('http://localhost:8080/sample/getAllUnderwriting', {
 				headers: headers
@@ -38,6 +46,71 @@ export class MapsComponent implements OnInit {
 				console.log(data);
         this.tableData.data = data
 		});
+    
+    
+    this.chartColor = "#FFFFFF";
+
+    this.canvas = document.getElementById("chartHours");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.chartHours = new Chart(this.ctx, {
+      type: 'line',
+
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct","Nov","Dec"],
+        datasets: [{
+            borderColor: "#FD26AA",
+            backgroundColor: "#BC72F3",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 1,
+            data: [11, 14, 8, 10, 15, 12, 8]
+          },
+         
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+
+        tooltips: {
+          enabled: false
+        },
+
+        scales: {
+          yAxes: [{
+
+            ticks: {
+              fontColor: "#9f9f9f",
+              beginAtZero: false,
+              maxTicksLimit: 5,
+              //padding: 20
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "#ccc",
+              color: 'rgba(255,255,255,0.05)'
+            }
+
+          }],
+
+          xAxes: [{
+            barPercentage: 3,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent",
+              display: false,
+            },
+            ticks: {
+              padding: 20,
+              fontColor: "#9f9f9f"
+            }
+          }]
+        },
+      }
+    });
   }
 
 
